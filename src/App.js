e65205d2b1c9737b405a7070ba628613334e132e
Lounas <lounasidjr@gmail.com>
2025-07-25 18:03:09 +0100

    move function

--- src/App.js
+++ src/App.js
@@ -92,9 +92,12 @@ function calculateWinner(squares){
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  
   currentSquare =history[history.length-1];
-  function handlePlay(nextSquare){
 
+  function handlePlay(nextSquare){
+     setHistory([...history,nextSquare]);
+     setXIsNext(!xIsNext);
   }
   return (
     <>
